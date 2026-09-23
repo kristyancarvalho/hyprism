5a8632493f75821965394cdcbdf9c6c5247a7891
Kristyan Carvalho <60079075+kristyancarvalho@users.noreply.github.com>
2026-09-22 23:35:11 -0300

    fix(hypr): scope scrolling to magic workspace

--- config/hypr/modules/general.lua
+++ config/hypr/modules/general.lua
@@ -3,7 +3,7 @@ hl.config({
         gaps_in = 4,
         gaps_out = 10,
         border_size = 0,
-        layout = "scrolling",
+        layout = "dwindle",
         resize_on_border = true,
         allow_tearing = false,
     },
